20d989c80e12dfddc675f3d2d6640d0066b4c6d8
o.kolomiichuk <lenafcdk@gmail.com>
2026-06-11 10:58:00 +0300

    added animations in title

--- src/Components/blockComponents/SectionTitle.jsx
+++ src/Components/blockComponents/SectionTitle.jsx
@@ -1,13 +1,75 @@
 import React from "react";
 
-function SectionTitle (props){
-    return(
-    <div style={{maxWidth: '700px', textAlign:'center', margin: '0 auto'}}>
-    <h2>{props.title}</h2>
-    <div style={{height:'3px', width:'90px', background:'#0096ff', margin: '0 auto'}}></div>
-    <div style={{height:'3px', width:'90px', background:'#0096ff', margin: '8px auto 0'}}></div>
-    <p>{props.subtitle}</p>
-    </div>
-    )
+function SectionTitle(props) {
+  return (
+    <>
+      <style>
+        {`.box{
+        position: relative;
+            width: 90px;
+            height: 10px;
+            margin: 0 auto 5px;
+            }
+        
+        .line{
+        position: absolute;
+              top: 4px;
+              left: 0;
+              width: 100%;
+              height: 3px;
+              background: #0096ff;
+        }
+
+        .point{
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 10px;
+              height: 10px;
+              background: #0096ff;
+              border-radius: 50%;
+              animation: slideBallToRight 1.5s linear infinite alternate;
+        }
+
+          @keyframes slideBallToRight {
+            from {
+              left: 0;
+            }
+            to {
+              left: calc(100% - 10px);
+            }
+          }
+
+          @keyframes slideBallToLeft {
+            from {
+              left: calc(100% - 10px);
+            }
+            to {
+              left: 0;
+            }
+          }
+        `}
+      </style>
+
+      <div style={{ maxWidth: "700px", textAlign: "center", margin: "0 auto" }}>
+        <h2>{props.title}</h2>
+
+        <div className="box">
+          <div className="line"/>
+          <div className="point"/>
+        </div>
+
+        <div className="box">
+          <div className="line"/>
+
+          <div  className="point"
+            style={{animation: 'slideBallToLeft 1.5s linear infinite alternate'}}/>
+        </div>
+
+        <p>{props.subtitle}</p>
+      </div>
+    </>
+  );
 }
-export default SectionTitle
+
+export default SectionTitle;
